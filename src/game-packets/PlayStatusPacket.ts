1ff20e0d9ae096147cbf0627a3baae55122c6b17
Dylan Ogle <delxhq@funo.network>
2019-01-20 03:16:14 +0000

    Few more packets.

--- src/game-packets/PlayStatusPacket.ts
+++ src/game-packets/PlayStatusPacket.ts
@@ -1,4 +1,8 @@
 import EncapsulatedPacket from "@/packets/EncapsulatedPacket"
+import Packet from "@/game-packets/Protocol";
 
 export default class PlayStatusPacket extends EncapsulatedPacket {
+    constructor() {
+        super(Packet.PLAY_STATUS_PACKET)
+    }
 }
