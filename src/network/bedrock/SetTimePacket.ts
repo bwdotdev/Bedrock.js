f3849a881a35422b457d17abf049d6e7fe7c1057
Dylan Ogle <delxhq@funo.network>
2019-01-24 15:34:48 +0000

    Hopefully setTimePacket.

---
+++ src/network/bedrock/SetTimePacket.ts
@@ -0,0 +1,18 @@
+import Packet from '@/network/bedrock/Protocol'
+import EncapsulatedPacket from '@/network/raknet/EncapsulatedPacket'
+import { BinaryStream } from '@/utils';
+
+export default class SetTimePacket extends EncapsulatedPacket {
+
+  public time: number
+
+  constructor(stream: BinaryStream) {
+    super(Packet.SET_TIME_PACKET, stream)
+
+    this.time = this.getStream().readInt();
+  }
+
+  protected encodeBody() {
+    this.getStream().writeInt(this.time)
+  }
+}
